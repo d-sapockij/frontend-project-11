efd30c903520ef551d0905e941c838ac0289b372
Daniil Sapockij <asapotsk@bk.ru>
2024-11-07 23:48:22 +0700

    Update src/view.js

--- src/js/view.js
+++ src/js/view.js
@@ -22,9 +22,19 @@ export default () => {
                     input.value = '';
                     input.focus();
                     break;
+                case 'sending':
+                    // запихать что-то типа блокировки формы на время отправки
+                    // пока он тут нужен чтобы onChange отрабатывал даже если state меняется на тот же самый 
+                    // error => error например
+                    // без этого кейс с error не отрабатывает каждый раз, только в первый
+                    // хорошо прослеживается на input.select()
+                    // отработает при ошибке только один раз, пока не сменится на другой state
+                    // ну и в целолм onChange работает при СМЕНЕ чего-то) так что все логично
+                    break;
                 case 'error':
                     input.classList.add('is-invalid');
                     input.focus();
+                    input.select();
                     break;
                 default:
                     throw new Error(`Unknown process state: ${value}`);
@@ -36,7 +46,7 @@ export default () => {
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-
+        watchedState.ui.processState = 'sending';
         const formData = new FormData(event.target);
         const url = formData.get('url');
         
