a9c90b0d46741437103be0a8caf5b949f3785310
d-sapockij <asapotsk@bk.ru>
2024-11-07 15:06:57 +0700

    Replace index.html and update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: './index.html',
         }),
     ],
     module: {
